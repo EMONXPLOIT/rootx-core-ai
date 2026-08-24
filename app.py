d41e5df35223961b6dc519040ec7137c1ffabfa5
EMONXPLOIT <emonkhan007emon@gmail.com>
2026-08-24 22:53:20 +0600

    Update app.py

--- app.py
+++ app.py
@@ -152,7 +152,7 @@ def ask_ai():
 
     try:
         response = client.models.generate_content(
-            model='gemini-2.5-flash',
+            model='gemini-1.5-flash',
             contents=user_prompt,
             config=types.GenerateContentConfig(
                 system_instruction=SYSTEM_INSTRUCTION,
